7b2a87e4d2bf9e0fda9f9c90593104f392e4cf32
YesidFonseca <y.fonseca@yupix.com>
2025-08-19 07:53:38 -0500

    Empty project

---
+++ Source/Cam2Android/Cam2Android.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cam2Android.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cam2Android, "Cam2Android" );
