37e74a072755950ff28ebfd1f62d478fa6598e4f
YesidFonseca <y.fonseca@yupix.com>
2025-09-29 14:13:50 -0500

    Second Stage

--- Source/Cam2Android/Private/QRCodeDetectionComp.cpp
+++ Source/Cam2Android/Private/QRCodeDetectionComp.cpp
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2025-2026 Yesid Fonseca
 
-
 #include "QRCodeDetectionComp.h"
-#include "AndroidCamera2Subsystem.h"
 #include "Kismet/GameplayStatics.h" 
+#include "Camera/CameraComponent.h"
+#include "AndroidCamera2Subsystem.h"
 #include "Engine/Engine.h"
 
 // Sets default values for this component's properties
@@ -22,9 +22,16 @@ UQRCodeDetectionComp::UQRCodeDetectionComp()
 void UQRCodeDetectionComp::BeginPlay()
 {
 	Super::BeginPlay();
+	LastFrameTimestamp = 0;
 
-	// ...
-	
+	LastCamPoses.SetNum(CameraSamples);
+	LastCamPosesTimes.SetNum(CameraSamples);
+	QRCodeCornersRay.SetNum(4);
+	FTriangulationSolver Solver = FTriangulationSolver();
+	Solver.LineMaxSamples = QRCodeTriangulationSolverSamples;
+	QRCodeCornerTSolvers.Init(Solver, 4);
+	EstimatedCornersLocations.SetNum(4);
+
 }
 
 
@@ -33,13 +40,47 @@ void UQRCodeDetectionComp::TickComponent(float DeltaTime, ELevelTick TickType, F
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	static bool bCamInitialized = false;
+	
+	static FAndroidCamera2Intrinsics CamLensIntrinsics = FAndroidCamera2Intrinsics();
+
+	static FQuat CamLensPoseRot = FQuat::Identity;
+
 	bool bGotFrame = false;
 	if (UGameInstance* GI = UGameplayStatics::GetGameInstance(GWorld))
 	{
 		if (auto* Cam2 = GI->GetSubsystem<UAndroidCamera2Subsystem>())
-		{
+		{		
 			if (Cam2->GetCameraState() == EAndroidCamera2State::INITIALIZED)
 			{
+				static int32 CameraSampleIdx = 0;
+				if (Camera)
+				{
+					FTransform CamHDM = Camera->GetComponentToWorld();
+					FRotator LeftEyeRot;
+					FVector LeftEyeLoc;
+					GetLeftEyeWorldPos(CamHDM, LeftEyeRot, LeftEyeLoc);
+					LastCamPoses[CameraSampleIdx] = FTransform(LeftEyeRot, LeftEyeLoc);
+					LastCamPosesTimes[CameraSampleIdx] = FPlatformTime::Cycles64();
+					CameraSampleIdx = (CameraSampleIdx + 1) % CameraSamples;
+				}
+
+				if(!bCamInitialized)
+				{
+					FAndroidCamera2LensPose CamLensPose;
+					Cam2->GetCameraIntrinsics(FString("50"), CamLensIntrinsics);
+					Cam2->GetCameraLensPose(FString("50"), CamLensPose);
+					CamLensPoseRot = FRotator(CamLensPose.OrientationUECoord.Rotator().Pitch, 0, 0).Quaternion();
+					bCamInitialized = true;
+				}
+
+				for (int32 i = 0; i < 4; i++)
+				{
+					EstimatedCornersLocations[i] = QRCodeCornerTSolvers[i].ComputeEstimatedPoint();
+				}
+				//UE_LOG(LogTemp, Display, TEXT("UQRCodeDetectionComp::TickComponent. EstP:%s, Delta:%s, Step:%f"), *EstimatedPoses[0].ToString(), *QRCodeCornerTSolvers[0].Delta.ToString(), QRCodeCornerTSolvers[0].Step);
+				
+
 				const uint8* YPtr = nullptr;
 				int32 YW = 0, YH = 0;
 				uint64 YTs = 0;
@@ -63,7 +104,6 @@ void UQRCodeDetectionComp::TickComponent(float DeltaTime, ELevelTick TickType, F
 					bGotFrame = true;
 
 					FQuircReader::DecodeFromLuma(YCurr.GetData(), Width, Height, Width, QRDetections);
-
 				}
 			}
 		}
@@ -78,6 +118,61 @@ void UQRCodeDetectionComp::TickComponent(float DeltaTime, ELevelTick TickType, F
 
 	if (OnQRCodeDetected.IsBound())
 		OnQRCodeDetected.Broadcast(QRDetections);
+
+	if(QRDetections.Num()>0)
+	{
+		UpdateCameraCachePose();
+
+		auto CameraCachePosePrevious = LastCamPoses[(CameraPoseIndex +CameraSamples-1) % CameraSamples];
+		const float PosDelta = FVector::Dist(CameraCachePose.GetLocation(), CameraCachePosePrevious.GetLocation());		
+		const float AngleRad = FQuat::ErrorAutoNormalize(CameraCachePose.GetRotation().GetNormalized(), CameraCachePosePrevious.GetRotation().GetNormalized()); // distancia angular en rad
+		const float AngleDeg = FMath::RadiansToDegrees(AngleRad);
+
+		//Avoid analizing when camera is moving too much
+		if (PosDelta > .05f || AngleDeg > .05f)
+			return;
+		//UE_LOG(LogTemp, Display, TEXT("UQRCodeDetectionComp::TickComponent. PosDelta:%f, AngleDeg: %f"), PosDelta, AngleDeg);
+
+		FRotator Rot = (CameraCachePose.Rotator().Quaternion() * CamLensPoseRot).Rotator();
+		for (int32 i = 0; i< QRDetections[0].Corners.Num(); i++)
+		{
+			auto Corner = QRDetections[0].Corners[i];
+			auto C = (FVector2f(Corner) - CamLensIntrinsics.PrincipalPoint)/CamLensIntrinsics.FocalLength;
+			FVector Ray = FVector(1, C.X, -C.Y);
+			Ray.Normalize();
+			Ray = Rot.RotateVector(Ray);
+			QRCodeCornersRay[i] = Ray;
+			
+		}
+
+		if (FVector::Dist(CameraCachePose.GetLocation(), PreviousCameraCachePoseLocation) < 5.f)
+		{
+			const float AngleDeg2 = (180.0) / UE_DOUBLE_PI * FMath::Acos(FVector::DotProduct(QRCodeCornersRay[0], PreviousCornerRay));
+			//Avoid analizing when QRCodeRay is not changing too much the angle
+			if(AngleDeg2<10.f)
+				return;
+		}
+
+		PreviousCameraCachePoseLocation = CameraCachePose.GetLocation();
+		PreviousCornerRay = QRCodeCornersRay[0];
+
+		for (int32 i = 0; i <4; i++)
+		{
+			QRCodeCornerTSolvers[i].AddLine(CameraCachePose.GetLocation(), QRCodeCornersRay[i]);
+		}
+
+		OnQRUsedForPoseEstimation();
+	}
+
+	
+
+	
+
+	
+}
+
+void UQRCodeDetectionComp::OnQRUsedForPoseEstimation_Implementation()
+{
 }
 
 
@@ -89,4 +184,73 @@ void UQRCodeDetectionComp::InitializeLuma(int32 InWidth, int32 InHeight)
 	Height = InHeight;
 
 	YCurr.SetNumZeroed(Width * Height);
+}
+
+void UQRCodeDetectionComp::UpdateCameraCachePose()
+{
+	// Search the camera pose with the nearest timestamp to LastFrameTimestamp
+	double MinDistanceTimeInSeconds = 1000000000.f;
+	double LastFrameTimestampInSeconds = FPlatformTime::ToSeconds64(LastFrameTimestamp);
+	double CameraPoseTimeInSeconds = 0;
+	int32 bestIdx = 0;
+	for (int i = 0; i < CameraSamples; i++)
+	{
+		CameraPoseTimeInSeconds = FPlatformTime::ToSeconds64(LastCamPosesTimes[i]);
+		double CurrentDistanceTimeInSeconds = FMath::Abs(CameraPoseTimeInSeconds - LastFrameTimestampInSeconds);
+		if (CurrentDistanceTimeInSeconds < MinDistanceTimeInSeconds)
+		{
+			bestIdx = i;
+			MinDistanceTimeInSeconds = CurrentDistanceTimeInSeconds;
+		}
+	}
+	//UE_LOG(LogTemp, Display, TEXT("UQRCodeDetectionComp::UpdateCameraCachePose. LastFrameTimestamp:%.6f, BestTimestamp:%.5f"), LastFrameTimestampInSeconds, FPlatformTime::ToSeconds64(LastCamPosesTimes[bestIdx]));
+	CameraCachePose = LastCamPoses[bestIdx];
+	CameraPoseIndex = bestIdx;
+}
+
+bool UQRCodeDetectionComp::GetNearestPointBetweenLines(FVector P1, FVector Dir1, FVector P2, FVector Dir2, FVector& NearesPoint)
+{
+	NearesPoint = FVector::ZeroVector;
+	Dir1.Normalize();
+	Dir2.Normalize();
+	FVector NormalPlane = FVector::CrossProduct(Dir1, Dir2);
+
+	FVector U = FVector::DotProduct(P1 - P2, NormalPlane)* NormalPlane + P2;
+
+	FVector B = FVector::DotProduct(U - P1, Dir2)* Dir2 + P1;
+
+	FVector AuxiliarNormalPlane = B - U;
+	AuxiliarNormalPlane.Normalize();
+
+	FPlane APlane(U, AuxiliarNormalPlane);
+	FVector RayDir = Dir1;
+
+	// Check ray is not parallel to plane
+	if ((RayDir | APlane) == 0.0f)
+	{
+		NearesPoint = FVector::ZeroVector;
+		return false;
+	}
+
+	float T = ((APlane.W - (P1 | APlane)) / (RayDir | APlane));
+
+	if (T < 0.f)
+		return false;
+
+	// Calculate intersection point
+	NearesPoint = P1 + RayDir * T;
+
+	return true;
+}
+
+
+void UQRCodeDetectionComp::GetLeftEyeWorldPos(FTransform HDM, FRotator& CamRot, FVector& CamLoc)
+{
+	if (GEngine && GEngine->StereoRenderingDevice.IsValid())
+	{
+		CamRot = HDM.GetRotation().Rotator();
+		CamLoc = HDM.GetLocation();
+		const float W2M = GetWorld()->GetWorldSettings()->WorldToMeters;
+		GEngine->StereoRenderingDevice->CalculateStereoViewOffset(EStereoscopicEye::eSSE_LEFT_EYE, CamRot, W2M, CamLoc);
+	}
 }
--- Source/Cam2Android/Public/QRCodeDetectionComp.h
+++ Source/Cam2Android/Public/QRCodeDetectionComp.h
@@ -10,7 +10,106 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQRCodeDetected, TArray<FQRDetection>, QRCodesDetected);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+USTRUCT(BlueprintType)
+struct FTriangulationSolver
+{
+	GENERATED_BODY();
+
+public:
+
+	
+	float Step;
+	FVector Delta;
+	FVector EstimatedPoint = FVector(1);
+	int32 LineMaxSamples = 5;
+
+	void AddLine(FVector LinePoint, FVector LineDirection)
+	{
+		if (LinesDirection.Num() < LineMaxSamples)
+		{
+			LineDirection.Normalize();
+			LinesDirection.Add(LineDirection);
+			LinesPoint.Add(LinePoint);
+			LineSampleIdx++;
+		}
+		else
+		{
+			LineSampleIdx = (LineSampleIdx + 1) % LineMaxSamples;
+			LineDirection.Normalize();
+			LinesDirection[LineSampleIdx] = LineDirection;
+			LinesPoint[LineSampleIdx] = LinePoint;
+		}
+
+		HessianMatrix = {};
+		for (int32 i = 0; i < LinesDirection.Num(); i++)
+		{
+			float x = LinesDirection[i].X, y = LinesDirection[i].Y, z = LinesDirection[i].Z;
+
+			HessianMatrix[0][0] -= x * x - 1.f;	HessianMatrix[0][1] -= x * y;		HessianMatrix[0][2] -= x * z;
+			HessianMatrix[1][0] -= y * x;		HessianMatrix[1][1] -= y * y - 1.f;	HessianMatrix[1][2] -= y * z;
+			HessianMatrix[2][0] -= z * x;		HessianMatrix[2][1] -= z * y;		HessianMatrix[2][2] -= z * z - 1.f;
+		}
+
+		int32 Precision = 3;
+		FString HessianString = FString::Printf(
+			TEXT("[[%.*f,%.*f,%.*f],[%.*f,%.*f,%.*f],[%.*f,%.*f,%.*f]]"),
+			Precision, HessianMatrix[0][0], Precision, HessianMatrix[0][1], Precision, HessianMatrix[0][2],
+			Precision, HessianMatrix[1][0], Precision, HessianMatrix[1][1], Precision, HessianMatrix[1][2],
+			Precision, HessianMatrix[2][0], Precision, HessianMatrix[2][1], Precision, HessianMatrix[2][2]
+		);
+
+		UE_LOG(LogTemp, Display, TEXT("FTriangulationSolver::AddLine. HessianMatrix: %s"), *HessianString);
+	}
+
+	FVector ComputeEstimatedPoint(float ModifierStep = 1.f)
+	{
+		ModifierStep = FMath::Clamp(ModifierStep, 0.001f, 1.f);
+		ComputeGradient();
+		EstimatedPoint = EstimatedPoint - FMath::Max(ModifierStep * Step, 0.1f) * Delta;
+		
+		return EstimatedPoint;
+	}
+
+private:
+	TArray<FVector> LinesPoint;
+	TArray<FVector> LinesDirection;
+	
+	int32 LineSampleIdx = 0;
+
+	std::array<std::array<double, 3>, 3> HessianMatrix;
+
+	
+	void ComputeGradient()
+	{
+		Delta = FVector::ZeroVector;
+		for (int32 i = 0; i < LinesDirection.Num(); i++)
+		{
+			FVector w = EstimatedPoint - LinesPoint[i];
+			float dot = FVector::DotProduct(w, LinesDirection[i]);
+			FVector proj = dot * LinesDirection[i];
+			FVector perp = w - proj;
+			Delta += perp;
+		}
+
+		const float x = Delta.X, y = Delta.Y, z = Delta.Z;
+		FVector MV = FVector(
+			HessianMatrix[0][0] * x + HessianMatrix[0][1] * y + HessianMatrix[0][2] * z,
+			HessianMatrix[1][0] * x + HessianMatrix[1][1] * y + HessianMatrix[1][2] * z,
+			HessianMatrix[2][0] * x + HessianMatrix[2][1] * y + HessianMatrix[2][2] * z
+		);
+
+		Step = x * MV.X + y* MV.Y + z * MV.Z;
+		if(Step>0.0001f)
+			Step = Delta.Length() * Delta.Length() / Step / 4.f;
+
+	}
+	
+	
+};
+
+class UCameraComponent;
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CAM2ANDROID_API UQRCodeDetectionComp : public UActorComponent
 {
 	GENERATED_BODY()
@@ -25,17 +124,43 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Quirc QRCode")
 	FOnQRCodeDetected OnQRCodeDetected;
 
+	UPROPERTY(EditAnywhere)
+	int32 CameraSamples = 30;
+
+	UPROPERTY(BlueprintReadWrite)
+	UCameraComponent* Camera;
+
+	
+		
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FVector> EstimatedCornersLocations;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FVector> QRCodeCornersRay;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnQRUsedForPoseEstimation();
+
+	UPROPERTY(BlueprintReadOnly)
+	FTransform CameraCachePose;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 QRCodeTriangulationSolverSamples = 3;
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	
+
 private:
 	void InitializeLuma(int32 InWidth, int32 InHeight);
+	
 	// Dimensiones (compactas)
 	int32 Width = 0;
 	int32 Height = 0;
@@ -45,5 +170,23 @@ private:
 	TArray<uint8> YCurr;
 
 	TArray<FQRDetection> QRDetections;
+
+	TArray<FTransform> LastCamPoses;
+
+	TArray<uint64> LastCamPosesTimes;
+
+	void UpdateCameraCachePose();
+
+	bool GetNearestPointBetweenLines(FVector P1, FVector Dir1, FVector P2, FVector Dir2, FVector& NearesPoint);
+
+	FVector PreviousCameraCachePoseLocation;
+
+	FVector PreviousCornerRay;
+
+	TArray<FTriangulationSolver> QRCodeCornerTSolvers;
+
+	int32 CameraPoseIndex = 0;
+	
+	void GetLeftEyeWorldPos(FTransform HDM, FRotator& CamRot, FVector& CamLoc);
 
 };
